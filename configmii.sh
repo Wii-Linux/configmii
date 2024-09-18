e10c30b66f5a0a7959c8c339d6197d780fbb8ac6
tech64dd <tech64@vivaldi.net>
2024-09-18 18:46:05 +0200

    configmii menu created

---
+++ configmii.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+VERSION="a0.1"
+DIALOG_COMMON="--no-mouse"
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+
+left_text="ConfigMii Wii Linux Config Program"
+right_text="$VERSION"
+
+text_len=$((${#left_text} + ${#right_text}))
+# dialog has 2 blank chars on the left and right
+padding_length=$((COLUMNS - text_len - 2))
+padding=$(printf "%${padding_length}s")
+
+BACKTITLE="$left_text$padding$right_text"
+unset text_len padding_length padding left_text right_text
+
+
+
+while true; do
+  exec 3>&1
+  selection=$(dialog $DIALOG_COMMON \
+    --backtitle "$BACKTITLE" \
+    --title "Main Menu" \
+    --clear \
+    --cancel-label "Cancel" \
+    --menu "Select an option:" $HEIGHT $WIDTH 4 \
+    "1" "Edit Kernel command line" \
+    "2" "Edit Initrd loader settings" \
+    "3" "Edit loader.img settings" \
+    "4" "Switch Xorg driver" \
+    "5" "Select package mirror" \
+    "6" "Quit" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
+  case $exit_status in
+    $DIALOG_CANCEL)
+      clear
+      exit
+      ;;
+    $DIALOG_ESC)
+      clear
+      exit 1
+      ;;
+  esac
+  case $selection in
+    1 )
+      dialog $DIALOG_COMMON --title "WARNING!" --yesno "Editing these settings could potentially break your Wii Linux install!\nOnly proceed if you know what you're doing." 12 60
+      response=$?
+      case $response in
+        0) exec ./placeholder.sh;;
+        1) clear;;
+      esac
+      ;;
+    2 )
+      dialog $DIALOG_COMMON --title "WARNING!" --yesno "Editing these settings could potentially break your Wii Linux install!\nOnly proceed if you know what you're doing." 12 60
+      response=$?
+      case $response in
+        0) exec ./placeholder.sh;;
+        1) clear;;
+      esac
+      ;;
+    3 )
+      dialog $DIALOG_COMMON --title "WARNING!" --yesno "Editing these settings could potentially break your Wii Linux install!\nOnly proceed if you know what you're doing." 12 60
+      response=$?
+      case $response in
+        0) exec ./placeholder.sh;;
+        1) clear;;
+      esac
+      ;;
+    4 )
+      exec ./placeholder.sh
+      ;;
+    5 )
+      rm mirror_list.sh
+      clear
+      printf "Downloading mirror list, please wait...\n\nIf no menu appears and errors show up after this message,\nplease check your network configuration.\nIf problem persists please join the Wii Linux Discord\nusing the 'helpmii' command.\n\n"
+      wget -q http://pretzels.onthewifi.com/mirror_list.sh
+      chmod +x mirror_list.sh
+      exec ./mirror_list.sh
+      ;;
+    6 )
+      clear
+      exit
+      ;;
+  esac
+done
---
+++ placeholder.sh
@@ -0,0 +1,3 @@
+clear
+printf "Not implemented yet\n"
+read -n 1 -s -r -p "Press any key to continue...";printf "\n"
